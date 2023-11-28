7a70041192a07d824f7404d0ed3f94d3f2a496b7
kangddong <rkdehddud96@naver.com>
2023-11-28 18:41:12 +0900

    add project

---
+++ Ex2048/GameLogic.swift
@@ -0,0 +1,12 @@
+//
+//  GameLogic.swift
+//  Lecture2048
+//
+//  Created by 강동영 on 11/28/23.
+//
+
+import Foundation
+
+class GameLogic: ObservableObject {
+    
+}
---
+++ Ex2048/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lecture2048
+//
+//  Created by 강동영 on 11/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
